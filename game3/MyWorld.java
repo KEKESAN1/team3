b646f643cb6aff15457737b1def740f5c2308432
KEKESAN1 <20065@hakodate.kosen-ac.jp>
2024-11-18 14:17:23 +0900

    (背景追加)

--- game3/MyWorld.java
+++ game3/MyWorld.java
@@ -1,21 +1,34 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
-/**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class MyWorld extends World
 {
+    GreenfootImage back;
+    GreenfootImage back_flop;
+    int back_dx = -3; // スクロール速度(マイナスにすると左から右)
+    int back_x = 0;
+    int back_width;
+    boolean flop = false;
 
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
     public MyWorld()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+    {
+        super(600, 330, 1); // 画像サイズをセットする
+        back = new GreenfootImage( "./images/back.png" );
+        back_flop = new GreenfootImage( "./images/back2.png" );
+        back_width = back.getWidth();
+    }
+
+    public void act()
+    {
+        back_x += back_dx;
+        if( back_x > 0){
+            back_x -= back_width;
+            flop = !flop;
+        }
+        if( back_x < -back_width ){
+            back_x += back_width;
+            flop = !flop;
+        }
+        getBackground().drawImage( flop ? back_flop : back, back_x, 0 );
+        getBackground().drawImage( flop ? back : back_flop, back_x+back_width, 0 );
     }
 }
